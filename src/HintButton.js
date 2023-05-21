e0a836d8d79be5fab197e1acbf4112dd7afa0930
Vishal <55916643+cmdvmd@users.noreply.github.com>
2023-05-21 13:36:32 -0400

    Fix issue in Hint Button

--- src/HintButton.js
+++ src/HintButton.js
@@ -3,19 +3,20 @@ const HintButton = ({cipher, plaintextWords, inputtedText, updateInputtedText}) 
     const spanish_hint_order = "AEIOUSNRLDTCMPBHQYVGFJZ\u00d1XKW";
 
     const giveHint = () => {
-        let given = false;
         let order = (cipher === "Xenocrypt") ? spanish_hint_order : hint_order;
         let newAnswer = [...inputtedText];
-        for (let index = 0; !given && index < order.length; index++) {
+        for (let index = 0; index < order.length; index++) {
+            let worked = false;
             let hint = order.charAt(index);
             plaintextWords.forEach((word, wordIndex) => {
                 for (let letterIndex = 0; letterIndex < word.length; letterIndex++) {
                     if (word.charAt(letterIndex) === hint && newAnswer[wordIndex][letterIndex] !== hint) {
                         newAnswer[wordIndex][letterIndex] = hint;
-                        given = true;
+                        worked = true;
                     }
                 }
             });
+            if (worked) break;
         }
         updateInputtedText(newAnswer);
     }
